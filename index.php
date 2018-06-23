e23376a35a27219f51978fa8d093e6f1d13e1b22
shimantoneer <poddernakla@gmail.com>
2018-06-23 23:34:43 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,2 @@
-<?php
-header("Location: https://www.investopedia.com/terms/d/digital-marketing.asp");
-die();
-?>
+
+<?php // Permanent 301 redirection header("HTTP/1.1 301 Moved Permanently"); header("Location: https://www.investopedia.com/terms/d/digital-marketing.asp"); exit(); ?> 
